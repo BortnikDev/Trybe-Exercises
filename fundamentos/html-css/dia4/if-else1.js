ed5a1f6b983335ac8007a80b59cea0e77b3d5152
Samuel Bortnik <samuelbortnik@gmail.com>
2022-11-24 00:00:25 +0200

    if-else

---
+++ fundamentos/html-css/dia4/if-else1.js
@@ -0,0 +1,8 @@
+let num1 = 20;
+let num2 = 30;
+
+if (num1 > num2){
+console.log(num1)
+} else{
+    console.log(num2)
+}
---
+++ fundamentos/html-css/dia4/if-else2.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 20;
+let c = 30;
+
+if (a > b && a > c){
+    console.log(b);
+} else if (b > a && b > c){
+    console.log(b);
+} else {
+    console.log(c);
+}
