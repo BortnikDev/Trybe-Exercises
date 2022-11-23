3fd95ea8fd2be95de5020237dc0a0dc78f400f48
Samuel Bortnik <samuelbortnik@gmail.com>
2022-11-23 23:39:57 +0200

    operation arithmetic

---
+++ fundamentos/html-css/dia4/exe-01.js
@@ -0,0 +1,17 @@
+const a = 5;
+const b = 10;
+
+let adicao = a + b;
+console.log(adicao);
+
+let subtracao = a - b;
+console.log(subtracao);
+
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+let divisao = a / b;
+console.log(divisao);
+
+let modulo = a % b;
+console.log(modulo);
